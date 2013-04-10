8d36a18740e6c99e1a30cf122d0e8330285ab451
Z.J <imunique.zj@gmail.com>
2013-04-10 21:54:59 +0800

    create TargetService

---
+++ MonkeyDart/src/loopz/monkeydart/service/ITargetService.java
@@ -0,0 +1,12 @@
+package loopz.monkeydart.service;
+
+import java.util.List;
+
+import loopz.monkeydart.domain.Target;
+
+public interface ITargetService {
+
+	public List<Target> findAllTarget();
+	
+	public List<Target> findTargetByCat(String category);
+}
---
+++ MonkeyDart/src/loopz/monkeydart/service/impl/TargetService.java
@@ -0,0 +1,22 @@
+package loopz.monkeydart.service.impl;
+
+import java.util.List;
+
+import loopz.monkeydart.domain.Target;
+import loopz.monkeydart.service.ITargetService;
+
+public class TargetService implements ITargetService {
+
+	@Override
+	public List<Target> findAllTarget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Target> findTargetByCat(String category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
