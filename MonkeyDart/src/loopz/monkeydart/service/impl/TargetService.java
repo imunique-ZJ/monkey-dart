75f07ac32477eb423c26d6754df26b7d04a81917
Z.J <imunique.zj@gmail.com>
2013-04-10 22:08:57 +0800

    實作查詢target

--- MonkeyDart/src/loopz/monkeydart/service/impl/TargetService.java
+++ MonkeyDart/src/loopz/monkeydart/service/impl/TargetService.java
@@ -1,22 +1,58 @@
 package loopz.monkeydart.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import loopz.monkeydart.db.DBConstants;
+import loopz.monkeydart.db.DBHelper;
 import loopz.monkeydart.domain.Target;
 import loopz.monkeydart.service.ITargetService;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 public class TargetService implements ITargetService {
 
+	private DBHelper dbHelper;
+	
+	public TargetService(Context context) {
+		dbHelper = new DBHelper(context);
+	}
+	
 	@Override
 	public List<Target> findAllTarget() {
-		// TODO Auto-generated method stub
-		return null;
+		SQLiteDatabase db = dbHelper.getReadableDatabase();
+		Cursor cursor = db.query(DBConstants.TABLE_TARGET, null, null, null, null, null, DBConstants.TARGET_ID);
+		
+		List<Target> list = new ArrayList<Target>();
+		while (cursor.moveToNext()) {
+			Target target = new Target();
+			target.setId(cursor.getInt(cursor.getColumnIndex(DBConstants.TARGET_ID)));
+			target.setValue(cursor.getString(cursor.getColumnIndex(DBConstants.TARGET_VALUE)));
+			target.setName(cursor.getString(cursor.getColumnIndex(DBConstants.TARGET_NAME)));
+			target.setCategory(cursor.getString(cursor.getColumnIndex(DBConstants.TARGET_CATEGORY)));
+			
+			list.add(target);
+		}
+		return list;
 	}
 
 	@Override
 	public List<Target> findTargetByCat(String category) {
-		// TODO Auto-generated method stub
-		return null;
+		SQLiteDatabase db = dbHelper.getReadableDatabase();
+		Cursor cursor = db.query(DBConstants.TABLE_TARGET, null, DBConstants.TARGET_CATEGORY + "=?", null, null, null, DBConstants.TARGET_ID);
+		
+		List<Target> list = new ArrayList<Target>();
+		while (cursor.moveToNext()) {
+			Target target = new Target();
+			target.setId(cursor.getInt(cursor.getColumnIndex(DBConstants.TARGET_ID)));
+			target.setValue(cursor.getString(cursor.getColumnIndex(DBConstants.TARGET_VALUE)));
+			target.setName(cursor.getString(cursor.getColumnIndex(DBConstants.TARGET_NAME)));
+			target.setCategory(cursor.getString(cursor.getColumnIndex(DBConstants.TARGET_CATEGORY)));
+			
+			list.add(target);
+		}
+		return list;
 	}
 
 }
