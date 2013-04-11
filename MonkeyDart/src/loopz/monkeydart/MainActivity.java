ac6864f615c683ceac1a8433fb58c7b58db50662
Z.J <imunique.zj@gmail.com>
2013-04-11 12:09:14 +0800

    IListAdaper加型態宣告

--- MonkeyDart/src/loopz/monkeydart/MainActivity.java
+++ MonkeyDart/src/loopz/monkeydart/MainActivity.java
@@ -3,6 +3,7 @@ package loopz.monkeydart;
 import loopz.monkeydart.async.TargetAsyncTask;
 import loopz.monkeydart.db.DBConstants;
 import loopz.monkeydart.db.DBHelper;
+import loopz.monkeydart.domain.Target;
 import loopz.monkeydart.listadapter.IListAdapter;
 import loopz.monkeydart.listadapter.impl.ListAdapterImpl;
 import android.app.Activity;
@@ -15,7 +16,7 @@ import android.widget.ListView;
 public class MainActivity extends Activity {
 	
 	private ListView targetLV;
-	private IListAdapter listAdapter;
+	private IListAdapter<Target> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
